cfb5a00ca4d8f125855f49322a4d1541970f9acd
johncooked <152047905+johncooked@users.noreply.github.com>
2024-05-03 02:36:55 +1000

    Setup express server

---
+++ cheeseria-backend/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cheeseRoutes = require("./routes/cheeseRoutes");
+const sequelize = require("./config/database");
+const seedCheeses = require("./config/seed");
+
+// Middleware setup
+app.use(express.json()); // This line enables JSON body parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Route setup
+app.use("/api/cheeses", cheeseRoutes);
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Database synchronization and seeding
+sequelize
+    .sync()
+    .then(async () => {
+        await seedCheeses(); // Execute seeding function after database synchronization
+        // Start the Express server
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database synchronization error:", err);
+    });
