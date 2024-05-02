7c7cea93b389a6b94eb21565032c1e9b683fc570
johncooked <152047905+johncooked@users.noreply.github.com>
2024-05-03 02:37:12 +1000

    Setup and populate database

---
+++ cheeseria-backend/src/config/seed.js
@@ -0,0 +1,50 @@
+const Cheese = require("../models/cheese");
+
+const seedCheeses = async () => {
+    try {
+        // Check if there are any cheeses in the database
+        const existingCheeses = await Cheese.findAll();
+        if (existingCheeses.length === 0) {
+            // If no cheeses are found, populate the database with sample data
+            await Cheese.bulkCreate([
+                {
+                    name: "Cheddar",
+                    image: "cheddar.jpg",
+                    pricePerKilo: 10.99,
+                    color: "Yellow",
+                },
+                {
+                    name: "Brie",
+                    image: "brie.jpg",
+                    pricePerKilo: 15.99,
+                    color: "White",
+                },
+                {
+                    name: "Gouda",
+                    image: "gouda.jpg",
+                    pricePerKilo: 12.99,
+                    color: "Yellow",
+                },
+                {
+                    name: "Blue Cheese",
+                    image: "blue_cheese.jpg",
+                    pricePerKilo: 20.99,
+                    color: "Blue",
+                },
+                {
+                    name: "Parmesan",
+                    image: "parmesan.jpg",
+                    pricePerKilo: 18.99,
+                    color: "Yellow",
+                },
+            ]);
+            console.log("Sample cheese data seeded successfully");
+        } else {
+            console.log("Cheeses already exist in the database");
+        }
+    } catch (error) {
+        console.error("Error seeding cheese data:", error);
+    }
+};
+
+module.exports = seedCheeses;
